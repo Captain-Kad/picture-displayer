452a295e47aa933e691b7f3066b650a095a97d76
Olubukunmi Kadri <buckskad@gmail.com>
2021-07-27 16:05:20 -0400

    Add files via upload

---
+++ picture_displayer/pic_displayer.js
@@ -0,0 +1,9 @@
+function changeImage(imgName){
+     image = document.getElementById('imgDisp');
+     image.src = imgName;
+  }
+
+  function clearImg(){
+    image = document.getElementById('imgDisp');
+    image.src = "";
+  }
